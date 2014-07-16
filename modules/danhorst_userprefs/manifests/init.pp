5c85a4555f58a2563179b8b4052fa94196335abf
danhorst <danhorst@puppetlabs.vm>
2014-07-16 13:58:40 +0000

    Simplifying the base class

--- modules/danhorst_userprefs/manifests/init.pp
+++ modules/danhorst_userprefs/manifests/init.pp
@@ -1,7 +1,3 @@
 include userprefs
 
-class danhorst_userprefs inherits userprefs {
-  file { "${homedir}/.vimrc":
-    source => 'puppet:///modules/danhorst_userprefs/vim/vimrc',
-  }
-}
+class danhorst_userprefs {}
