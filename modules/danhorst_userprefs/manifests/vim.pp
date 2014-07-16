de50606594ec2175c6894c9f2008c4da9244da94
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2014-07-16 17:52:31 +0000

    Correcting the same problem

--- modules/danhorst_userprefs/manifests/vim.pp
+++ modules/danhorst_userprefs/manifests/vim.pp
@@ -1,6 +1,6 @@
 class danhorst_userprefs::vim inherits userprefs::vim {
   File["${userprefs::vim::homedir}/.vimrc"] {
     source => 'puppet:///modules/danhorst_userprefs/vim/vimrc',
-    user   => 'root',
+    owner  => 'root',
   }
 }
