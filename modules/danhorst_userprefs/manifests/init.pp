799486587a3f9b670cff5d56e2ce349cb4208a6c
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2014-07-16 20:14:43 +0000

    Adding a note to origin

--- modules/danhorst_userprefs/manifests/init.pp
+++ modules/danhorst_userprefs/manifests/init.pp
@@ -1,3 +1,6 @@
+# This class extends the userprefs module provided by the puppet fundamentals course
+# https://github.com/puppetlabs/puppetlabs-training-bootstrap/tree/master/modules/userprefs
+
 class danhorst_userprefs {
   file { "${$userprefs::profile::homedir}/.gitconfig" :
     ensure => file,
