f8b7ce09e541ddacc30cf39edef57f3dee3ab713
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2014-07-16 19:10:38 +0000

    10.3: out-selectors

--- modules/conditionals/manifests/in.pp
+++ modules/conditionals/manifests/in.pp
@@ -4,6 +4,7 @@ class conditionals::in {
     content => $::operatingsystem ? {
       'CentOS' => 'You are running CentOS and saving money.',
       'RHEL'   => 'You are running RHEL and spending money.',
+      default  => 'Operating system is unknown',
     },
   }
 }
