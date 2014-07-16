1a175ba932afa4557e4028b8fc7116b4e5c5c728
danhorst <danhorst@puppetlabs.vm>
2014-07-16 15:06:12 +0000

    Explicitly declare that gitconfig is a file

--- modules/danhorst_userprefs/manifests/init.pp
+++ modules/danhorst_userprefs/manifests/init.pp
@@ -2,7 +2,7 @@ include userprefs
 
 class danhorst_userprefs {
   file { "${homedir}/.gitconfig":
-    ensure => present,
+    ensure => file,
     source => 'puppet:///modules/danhorst_userprefs/gitconfig',
     group  => 'pe-puppet',
     mode   => 0444,
