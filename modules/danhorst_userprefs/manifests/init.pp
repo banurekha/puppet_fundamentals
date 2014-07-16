fe50ead7bb62851fc45642fea8d8e484ee854f74
danhorst <danhorst@puppetlabs.vm>
2014-07-16 14:49:00 +0000

    Fixing group name typo

--- modules/danhorst_userprefs/manifests/init.pp
+++ modules/danhorst_userprefs/manifests/init.pp
@@ -4,7 +4,7 @@ class danhorst_userprefs {
   file { "${homedir}/.gitconfig":
     ensure => present,
     source => 'puppet:///modules/danhorst_userprefs/gitconfig',
-    group  => 'pre-puppet',
+    group  => 'pe-puppet',
     mode   => 0444,
   }
 }
