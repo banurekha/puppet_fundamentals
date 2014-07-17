1b161eb6d255a6e9432fc5bec7cfe3019bfd16c6
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2014-07-17 02:13:04 +0000

    Create the user and group that wordpress expects

--- modules/profiles/manifests/wordpress.pp
+++ modules/profiles/manifests/wordpress.pp
@@ -1,10 +1,22 @@
 class profiles::wordpress(
   $db_password = 'use_hiera',
+  $db_user     = 'wordpress',
+  $wp_group    = 'wordpress',
+  $wp_owner    = 'wordpress',
 ){
+  user { $wp_owner:
+    ensure => present,
+    gid    => $wp_group,
+  }
+
+  group { $wp_group:
+    ensure => present,
+  }
+
   class { '::wordpress':
-    wp_owner    => 'wordpress',
-    wp_group    => 'wordpress',
-    db_user     => 'wordpress',
     db_password => $db_password,
+    db_user     => $db_user,
+    wp_group    => $wp_group,
+    wp_owner    => $wp_owner,
   }
 }
